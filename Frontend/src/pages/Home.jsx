69f59a3216570be3591876e6c615277e38e93cff
realsudarshan <sudarshandhakal40@gmail.com>
2025-06-19 18:15:24 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -16,11 +16,16 @@ import img11 from '../images/img11.jpg';
 import img12 from '../images/img12.jpg';
 import img13 from '../images/img13.jpg';
 import img14 from '../images/img14.jpg';
+import img15 from '../images/img15.jpg';
+import img16 from '../images/img16.jpg';
+import img17 from '../images/img17.jpg';
+import img18 from '../images/img18.jpg';
+import img19 from '../images/img19.jpg';
 
 import heroImg from '../images/heroImg.jpg'; // NEW featured image
 
 const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,img11,
-  img12,img13,img14,
+  img12,img13,img14,img15,img16,img17,img18,img19,
 ];
 
 export default function Home() {
