9e4ede7625f51776eb3010af685cf67b485d3642
realsudarshan <sudarshandhakal40@gmail.com>
2025-06-20 17:40:02 +0530

    Update blogRoutes.js

--- Backend/routes/blogRoutes.js
+++ Backend/routes/blogRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllBlogs, getBlogById, createBlog } = require('../controllers/blogController');
+const { getAllBlogs, getBlogById, createBlog, deleteBlog, updateBlog  } = require('../controllers/blogController');
 const router = express.Router();
 
 router.get('/', getAllBlogs);
 router.get('/:id', getBlogById);
 router.post('/', createBlog);
+router.delete('/:id', deleteBlog);
+router.put('/:id', updateBlog);
 
 module.exports = router;
