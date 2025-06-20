b38ebe7a418339d8cac5b479e5bdf3beb17ff55a
realsudarshan <sudarshandhakal40@gmail.com>
2025-06-20 08:15:24 +0530

    Create blogRoutes.js

---
+++ Backend/routes/blogRoutes.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const { getAllBlogs, getBlogById, createBlog } = require('../controllers/blogController');
+
+// const router = express.Router();
+
+// router.get('/', getAllBlogs);
+// router.get('/:id', getBlogById);
+// router.post('/', createBlog);
+
+// module.exports = router;
